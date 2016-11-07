3f8b7b8b45e7799320d6d04962558b9c05bc7ab4
Alek <alekhrycaiko@gmail.com>
2016-11-07 09:55:23 -0800

    Additional parsing work for creating recursive tree iteration

--- src/controller/HtmlParserUtility.ts
+++ src/controller/HtmlParserUtility.ts
@@ -1,3 +1,4 @@
+import {ASTNode} from "parse5";
 /**
  * Created by alekspc on 2016-11-05.
  */
@@ -13,6 +14,7 @@ export default class HtmlParserUtility {
         Therefore we are making a rooms.json file to store all the rooms information.
       */
 
+
     /*
         When an html file is passed into the datasetcontroller, determineValidBuildingList will parse that file and create
         a list of valid buildings using their shortname.
@@ -24,7 +26,13 @@ export default class HtmlParserUtility {
   //      for (let key in src) {
     //        console.log(key);
       //  }
-        var document = parse5.parse(json, {locatinInfo : true});
+        var that = this;
+        var document:ASTNode = parse5.parse(json, {locatinInfo : true});
+        // needs to iterate for TD, needs to skip TH.
+        let tempArray:Array<string>;
+
+        let out: Array<String> = that.iterateNodeList('class', '.views-field-title', document, tempArray);
+        console.log(out);
 
         // basically how this is going to work:
         // (1) We need to find all keys for each childNode, then iterate through them
@@ -32,13 +40,12 @@ export default class HtmlParserUtility {
         // (3) We then need to iterate through each elvel of attributes checking for our value
         // (4) We then need to, again, iterate through every childNode by checking its count etc.. (basically looping at this point).
         // iterator or recursion should work...
-        
+
 
         var documentHtml = parse5.serialize(document);
 
-        while(true){
-            // if our current level of childnodes > 0
-        }
+        return 0;
+
         /*
         let rawNodeList= document.querySelectorAll('td.views-field-field-building-code');
         let validBuildingList : Array<String>;
@@ -47,7 +54,36 @@ export default class HtmlParserUtility {
         }
         */
     }
+    /*
+        TODO: Finds the value of a node that has a certain html id/class
+     */
+    public iterateNodeList(id: string, value: string, nodeList: ASTNode, tempArray : Array<string>) : Array<string> {
 
+        if (!nodeList.childNodes && !nodeList.attrs) {
+            return tempArray;
+        }
+
+        if (nodeList.attrs && nodeList.attrs.length > 0){
+            // check all the attributes. if I find one lets push it on the stack.
+            // if attrs is zero and children are zero, return?
+            let numberOfAttributeObjects = nodeList.attrs.length;
+            for (var i=0 ; i <numberOfAttributeObjects; i++){
+                console.log(nodeList.attrs[i].name === "class");
+                console.log(nodeList.attrs[i].value === "views-field-field-building-code");
+                if (nodeList.attrs[i].name === "class" && nodeList.attrs[i].value.indexOf('views-field-field-building-code') > -1){
+                    console.log('hurrah');
+                    // now if we take the child Node and take its text values etc, we get a singular value.
+                    // problem now: How do we take this concept and iterate over the entire tree for all levels?
+                }
+            }
+        }
+        if (nodeList.childNodes && nodeList.childNodes.length > 0){
+            let lengthOfChildNodes = nodeList.childNodes.length;
+            for (var i=0; i <lengthOfChildNodes ; i++){
+                this.iterateNodeList(id, value, nodeList.childNodes[i], tempArray);
+            }
+        }
+    }
     /*
         TODO: Returns buildings shortname
      */
