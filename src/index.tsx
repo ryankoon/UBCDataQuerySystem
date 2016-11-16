a1b4461e5c37be0156043e097d796e83638f9e5f
Alek <alekhrycaiko@gmail.com>
2016-11-16 08:48:33 -0800

    Setup for react and webpack

---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+/**
+ * Created by alekspc on 2016-11-16.
+ */
+/*
+TODO: REPLACE ALL THIS. THIS IS TEMPORARY SETUP FOR WEBPACK/REACT
+ */
+
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import { Hello } from "./components/Hello";
+
+ReactDOM.render(
+    <Hello compiler="TypeScript" framework="React" />,
+    document.getElementById("example"
+    )
+)
+
