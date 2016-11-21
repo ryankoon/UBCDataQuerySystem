3ce14d6254e3b53377c54eb3a12f5d5ad8463644
Alek <alekhrycaiko@gmail.com>
2016-11-20 22:34:59 -0800

    updated webpack and pckg json

--- src/components/tabs.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-//TODO: REPLACE ALL THIS. THIS IS TEMPORARY SETUP FOR WEBPACK/REACT
-
-export class Tabs extends React.Component<any, any> {
-    render() {
-        return <div>Hello world this is react setup!!</div>
-    }
-}
