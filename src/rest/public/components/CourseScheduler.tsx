73214e6f4efbb7b5192feb4e89988a21e0e90e7c
Alek <alekhrycaiko@gmail.com>
2016-11-28 20:36:51 -0800

    Added stubs for the course explorer

--- src/rest/public/components/CourseScheduler.tsx
+++ src/rest/public/components/CourseScheduler.tsx
@@ -5,19 +5,71 @@
  Should export a rendered module of dropdowns populated with rooms.
  */
 import * as React from 'react';
-import {CourseSchedulerForm} from "./CourseSchedulerForm";
+import {ResponseHandler} from './ResponseHandler';
+import {Alerts} from './Alert';
+import * as request from 'superagent';
+import * as superagent from "superagent";
 
 export class CourseScheduler extends React.Component<any, any> {
     constructor(props:any) {
         super(props);
         this.state = {
+            schedule : false,
+            responseContent : null,
+            responseKeys : null,
+            errorMessage : null
+        }
+    }
+    handleResponse(res : any, payload : any){
+        let result = res.body.result;
+        // TODO: ensure set keys works.
+        let resultKeys = Object.keys(result);
+        this.setState({
+            schedule : true,
+            responseContent : result,
+            responseKeys : resultKeys
+        });
+    }
+
+
+    componentWillMount(){
+        if (localStorage.getItem('rooms') !== null && localStorage.getItem('courses') !== null){
+            // TODO: set post
+            let rooms  = localStorage.getItem('rooms');
+            let courses = localStorage.getItem('courses');
+
+            let payload = {
+                rooms : rooms,
+                courses : courses
+            }
+            superagent.post('/address')
+                .send(payload)
+                .end( (err, res) => {
+                    if(err){
+                        console.log(err);
+                    }
+                    if(res){
+                        this.handleResponse(res, payload);
+                    }
+                });
+        }else{
+            this.setState({
+                schedule : false,
+                errorMessage : 'Please select rooms and courses before submitting',
+            });
+
         }
     }
     render() {
-    return (
-        <div>
-            <CourseSchedulerForm compiler="TypeScript" framework="React"/>
-        </div>
-    )
+        if (this.state.schedule === true) {
+            return (
+                <div>
+                    <ResponseHandler  compiler="TypeScript" framework="React"/>
+                </div>
+            )
+        }
+        else{
+            return(<Alerts alertStyle="danger"  message = {this.state.errorMessage} ></Alerts>)
+        }
     }
 }
